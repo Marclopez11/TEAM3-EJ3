ecd386f699db8ff15a926a48eca496955b499a9d
Marc Lopez <marclopezmarco469@gmail.com>
2022-04-20 14:32:03 +0200

    Crear libros y compararlos

--- TEAM3-EJ3/src/ej3/Ej3App.java
+++ TEAM3-EJ3/src/ej3/Ej3App.java
@@ -9,6 +9,34 @@ public class Ej3App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Creamos el libro 1
+		Libro libro = new Libro("978-84-18089-11-4", "Guerra de liberación", "Franco Bahamonde, Francisco", 160);
+		System.out.println("Libro 1");
+		System.out.println(libro);
+
+		
+		//Creamos el libro 2
+		Libro libro2 = new Libro("978-84-08-24702-9", "Enciclopedia nazi", "Eslava Galán, Juan", 120);
+		System.out.println("Libro 2");
+		System.out.println(libro2);
+		
+		
+		//comparamos las páginas
+		compararPaginas(libro,libro2);
+
+	}
+	
+	
+	//Comparamos recogiendo el valor de las paginas de cada libro
+	public static void compararPaginas(Libro x, Libro y) {
+		
+		if(x.getNumeroPaginas() > y.getNumeroPaginas()) {
+			System.out.println(x.getTitulo()+" tiene "+x.getNumeroPaginas()+" páginas. Tiene mas páginas que "+y.getTitulo());
+		}else {
+			System.out.println(y.getTitulo()+" tiene "+y.getNumeroPaginas()+" páginas. Tiene mas páginas que "+x.getTitulo());
+
+		}
+		
 	}
 
 }
